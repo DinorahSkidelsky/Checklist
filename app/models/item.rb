a8e50cbacbd00b1129428018145d012432ca4913
Dinorah Skidelsky <dinorahskidelsky@gmail.com>
2022-07-20 19:14:32 -0300

    Progress in the app

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
+  belongs_to :user
   validates :title, :description, presence: true
+
+  def completed?
+    !completed_at.blank?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_18_211520) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_20_205458) do
   create_table "items", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.datetime "completed_at"
   end
 
   create_table "users", force: :cascade do |t|
